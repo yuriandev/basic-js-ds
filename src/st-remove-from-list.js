e507266ea6db0c2a53417a80436e4933b06fe4a6
Yuri An <yuriandev@gmail.com>
2021-10-13 20:09:41 +0300

    feat: solution for task 'st-remove-from-list'

--- src/st-remove-from-list.js
+++ src/st-remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -19,7 +19,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  */
 
-module.exports = function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+module.exports = function removeKFromList(l, k) {
+  if (l.value === k) {
+    l = l.next;
+  }
+
+  let current = l;
+  let nextNode = current.next;
+
+  while (nextNode) {
+    if (nextNode.value === k) {
+      current.next = nextNode.next;
+    } else {
+      current = current.next;
+    }
+    nextNode = current.next;
+  }
+
+  return l;
+};
+
+// module.exports = function removeKFromList(l, k) {
+//   if (l.value === k) {
+//     l = l.next;
+//   }
+
+//   let node = l;
+//   let nextNode = node.next;
+
+//   while (nextNode) {
+//     if (nextNode.value === k) {
+//       node.next = nextNode.next;
+
+//       if (node.next === null) break;
+//     }
+
+//     node = node.next;
+//     nextNode = node.next;
+//   }
+
+//   return l;
+// };
